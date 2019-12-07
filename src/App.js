d859ea8c1f2a6ed4c014bb6ce2624c6f6725c73e
alexlo1 <alexdlo8967@gmail.com>
2019-12-06 20:41:49 -0800

    Styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './knight-icon.svg';
 import './App.css';
 import Game from './Game.js';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <div className="app-main">
-          <img src={logo} className="app-logo" alt="logo" />
-          <h1 className="app-title">Welcome to Mr. Knight's Wild Tour</h1>
-          <Game />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <img src={logo} className="app-logo" alt="logo" />
+    <h1 className="app-title">Welcome to Mr. Knight's Wild Tour</h1>
+    <Game />
+  </div>
+);
+
+export default App;
+
